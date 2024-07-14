3c6b787d8341ee78bb26399253993e8fda36b97d
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-07-14 17:41:13 +0700

    shadcn stuff & dropdown

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,34 @@ import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import Head from "next/head";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <div className={GeistSans.className}>
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
+          <SessionProvider session={session}>
+            <div className={GeistSans.className}>
+              <Component {...pageProps} />
+            </div>
+          </SessionProvider>
+        </ThemeProvider>
+      </main>
+    </>
   );
 };
 
