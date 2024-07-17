c66e5e1dbc33ba6bfdb36ab1986d08e3cf260a72
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-07-17 20:15:22 +0700

    fix positioning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main>
       <Navbar />
       <section className="w-full h-dvh flex flex-col justify-center">
-        <div className="grid grid-rows-2 md:grid-cols-2 sm:grid-rows-1">
+        <div className="grid grid-rows-2 sm:grid-cols-2 sm:grid-rows-1">
           <div className="flex flex-col justify-center items-center sm:hidden">
             <IconSVG className="w-[16rem] h-[16rem] md:w-[24rem] md:h-[24rem] xl:w-[32rem] xl:h-[32rem] 2xl:w-[48rem] 2xl:h-[48rem]"/>
           </div>
