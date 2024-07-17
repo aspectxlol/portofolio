9e3de4a50bf89f4786e012b659986cba521a693e
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-07-17 18:37:11 +0700

    index sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,16 @@ export default function Home() {
     <main>
       <Navbar />
       <section className="w-full h-dvh flex flex-col justify-center">
-        <div className="grid grid-cols-2">
-          <div className="flex flex-col justify-center items-center">
-            <h1 className="text-7xl font-bold">Louie Hansen</h1>
-            <p className="">Programmer, Developer, Student.</p>
+        <div className="grid grid-rows-2 md:grid-cols-2 md:grid-rows-1">
+          <div className="flex flex-col justify-center items-center md:hidden">
+            <IconSVG className="w-[16rem] h-[16rem] md:w-[24rem] md:h-[24rem] xl:w-[32rem] xl:h-[32rem] 2xl:w-[48rem] 2xl:h-[48rem]"/>
           </div>
           <div className="flex flex-col justify-center items-center">
-            <IconSVG className="w-[32rem] h-[32rem]"/>
+            <h1 className="text-5xl font-bold text-center md:text-7xl xl:text-9xl 2xl:text-[10rem]">Louie Hansen</h1>
+            <p className="text-xl md:text-xl xl:text-3xl 2xl:text-5xl text-center">Programmer, Developer, Student.</p>
+          </div>
+          <div className="hidden flex-col justify-center items-center md:flex">
+            <IconSVG className="w-[16rem] h-[16rem] md:w-[24rem] md:h-[24rem] xl:w-[32rem] xl:h-[32rem] 2xl:w-[48rem] 2xl:h-[48rem]"/>
           </div>
         </div>
       </section>
